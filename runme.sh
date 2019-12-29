81b17c3a30adc9a7f0ce5ac5743278a5b9c1d86d
Andrew Kozhokaru <andrew@kozhokaru.com>
2019-12-29 08:39:34 -0800

    Add check that VPN is alive before lounching download station.

--- runme.sh
+++ runme.sh
@@ -50,7 +50,9 @@ END
   echo "Address before reconnect: ${curr_ip} (timed out if empty)"
   echo "New address: ${address}"
 
-  /volume1/\@appstore/DownloadStation/scripts/S25download.sh start > /dev/null
+  if [[ "${address}" != "${home_ip}" ]]; then
+    /volume1/\@appstore/DownloadStation/scripts/S25download.sh start > /dev/null
+  fi
 
   exit 123
 }
