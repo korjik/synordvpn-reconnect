63b8d98ad20c348e0f86e855b92f19ad849f3fa5
Andrew Kozhokaru <andrew@.kozhokaru.com>
2019-12-29 20:04:10 -0800

    Fix error

--- download-station-killswitch.sh
+++ download-station-killswitch.sh
@@ -3,7 +3,7 @@
 curr_ip=$(curl -s --max-time 10 https://ifconfig.io)
 home_ip=$(curl -s --interface eth0 https://ifconfig.io)
 
-if [[ "${curr_ip}" == "${home_ip}" ]]; 
+if [[ "${curr_ip}" == "${home_ip}" ]]; then 
   echo 'VPN is not working.'
 
   /volume1/\@appstore/DownloadStation/scripts/S25download.sh stop > /dev/null
