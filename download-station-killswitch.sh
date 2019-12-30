c3eacaef173919041e918372ae8b355d00f84eec
Andrew Kozhokaru <andrew@.kozhokaru.com>
2019-12-29 20:32:37 -0800

    Add checks functionality

--- download-station-killswitch.sh
+++ download-station-killswitch.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-curr_ip=$(curl -s --max-time 10 https://ifconfig.io)
-home_ip=$(curl -s --interface eth0 https://ifconfig.io)
+function checkds {
+  ps axu | grep 'DownloadStation download' | grep -v grep
+}
 
-if [[ "${curr_ip}" == "${home_ip}" ]]; then 
-  echo 'VPN is not working.'
+if [[ $(checkds) ]]; then
+  curr_ip=$(curl -s --max-time 10 https://ifconfig.io)
+  home_ip=$(curl -s --interface eth0 https://ifconfig.io)
 
-  /volume1/\@appstore/DownloadStation/scripts/S25download.sh stop > /dev/null
-  if [[ $(ps aux | grep DownloadStation) ]]; then
-    echo 'Warning: Download station is working! Was not able to shut it down.'
-  else
-    echo 'Download station is shut down.'
+  if [[ "${curr_ip}" == "${home_ip}" ]]; then 
+    echo -e 'VPN is not working.\nShutting down Download station...'
+    
+    /volume1/\@appstore/DownloadStation/scripts/S25download.sh stop > /dev/null
+	sleep 3
+    if [[ $(checkds) ]]; then
+      echo 'Warning: Download station is working! Was not able to shut it down.'
+    else
+      echo 'Download station is shut down.'
+    fi
+    exit 456
   fi
-  exit 456
 fi
