c716991862f9fd13ed87cfde6abc4b8f35959520
Andrew Kozhokaru <andrew@kozhokaru.com>
2019-12-29 19:54:12 -0800

    Add more output information and throwing error

--- download-station-killswitch.sh
+++ download-station-killswitch.sh
@@ -4,5 +4,13 @@ curr_ip=$(curl -s --max-time 10 https://ifconfig.io)
 home_ip=$(curl -s --interface eth0 https://ifconfig.io)
 
 if [[ "${curr_ip}" == "${home_ip}" ]]; 
+  echo 'VPN is not working.'
+
   /volume1/\@appstore/DownloadStation/scripts/S25download.sh stop > /dev/null
+  if [[ $(ps aux | grep DownloadStation) ]]; then
+    echo 'Warning: Download station is working! Was not able to shut it down.'
+  else
+    echo 'Download station is shut down.'
+  fi
+  exit 456
 fi
